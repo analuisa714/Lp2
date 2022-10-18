eb968290694a873ce8f29f97d72b9effad5084c3
Ana Luisa Oliveira da Silva <96425830+analuisa714@users.noreply.github.com>
2022-10-18 08:58:16 -0300

    Delete src directory
    excluindo arquivos que não são mais necessários

--- src/mainCreditInquiry.java
+++
@@ -1,6 +0,0 @@
-public class mainCreditInquiry {
-     public static void main(String[] args) throws Exception { 
-        creditInquiry aplication = new creditInquiry();
-        aplication.processRequest();
-    } 
-}
